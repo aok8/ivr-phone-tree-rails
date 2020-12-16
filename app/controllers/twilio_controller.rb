e582c3f7c6c6c83badedee84ef4d3d152e33f158
Alain Kouassi <aokouassi@gmail.com>
2020-12-16 16:13:51 -0500

    Got first call going through

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,8 +1,9 @@
 require 'twilio-ruby'
 require 'sanitize'
 
-
 class TwilioController < ApplicationController
+  
+
 
   def index
     render text: "Dial Me."
@@ -12,8 +13,8 @@ class TwilioController < ApplicationController
   def ivr_welcome
     response = Twilio::TwiML::VoiceResponse.new
     gather = Twilio::TwiML::Gather.new(num_digits: '1', action: menu_path)
-    gather.say("Thanks for calling the E T Phone Home Service. Please press 1 for
-    directions. Press 2 for a list of planets to call.", loop: 3)
+    gather.say("Thanks for calling the Household Phone Service. Please press 1 for
+    information and instructions. Press 2 to be transfered to the next available number", loop: 3)
     response.append(gather)
 
     render xml: response.to_s
@@ -25,53 +26,19 @@ class TwilioController < ApplicationController
 
     case user_selection
     when "1"
-      @output = "To get to your extraction point, get on your bike and go down
-        the street. Then Left down an alley. Avoid the police cars. Turn left
-        into an unfinished housing development. Fly over the roadblock. Go
-        passed the moon. Soon after you will see your mother ship."
+      @output = "This phone routing service is to make reaching the most available responsible party."
       twiml_say(@output, true)
     when "2"
-      list_planets
+      phone_tree
     else
       @output = "Returning to the main menu."
       twiml_say(@output)
     end
 
-  end
-
-  # POST/GET ivr/planets
-  # planets_path
-  def planet_selection
-    user_selection = params[:Digits]
-
-    case user_selection
-    when "2"
-      twiml_dial("+12024173378")
-    when "3"
-      twiml_dial("+12027336386")
-    when "4"
-      twiml_dial("+12027336637")
-    else
-      @output = "Returning to the main menu."
-      twiml_say(@output)
-    end
   end
 
   private
 
-  def list_planets
-    message = "To call the planet Broh doe As O G, press 2. To call the planet
-    DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To
-    go back to the main menu, press the star key."
-
-    response = Twilio::TwiML::VoiceResponse.new do |r|
-      gather = Twilio::TwiML::Gather.new(num_digits: '1', action: planets_path)
-      gather.say(message, voice: 'alice', language: 'en-GB', loop: 3)
-      r.append(gather)
-    end
-
-    render xml: response.to_s
-  end
 
   def twiml_say(phrase, exit = false)
     # Respond with some TwiML and say something.
@@ -79,8 +46,7 @@ class TwilioController < ApplicationController
     response = Twilio::TwiML::VoiceResponse.new do |r|
       r.say(phrase, voice: 'alice', language: 'en-GB')
       if exit
-        r.say("Thank you for calling the ET Phone Home Service - the
-        adventurous alien's first choice in intergalactic travel.")
+        r.say("Thanks for calling the Household Phone Service.")
         r.hangup
       else
         r.redirect(welcome_path)
@@ -97,4 +63,13 @@ class TwilioController < ApplicationController
 
     render xml: response.to_s
   end
+end
+
+def phone_tree
+  first_call = "praneet"
+  response = Twilio::TwiML::VoiceResponse.new
+  response.say("Connecting you to #{first_call}. ")
+  response.dial(number: "+16308533980",
+                action: "/ivr/welcome")
+  render xml: response.to_s
 end
